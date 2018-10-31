6a136cd48781e43c6dd9afffd390e353431dc2c3
NicoP <polt.gun@gmail.com>
2018-10-31 10:14:57 +0100

    fix maxz in simplex (working)

--- test/archetype-quickstart-jdk8/src/main/java/test/Simplex.java
+++ test/archetype-quickstart-jdk8/src/main/java/test/Simplex.java
@@ -21,44 +21,46 @@ public class Simplex {
         lpw.setMinProblem(false);
         for (int i = 0; i < graph.getLength(); i++) {
             lpw.plus("z" + i, graph.getValue(i));
-            ;// zi stands for does the node i was visited
+            // zi stands for does the node i was visited
 
             for (int j = 1; j < maxSize; j++) {
-                LPWizardConstraint tmp = lpw.addConstraint("access" + i + "," + j, 0, "<=");
-                tmp.plus("x" + i + "," + j, -1).setAllVariablesBoolean();
+                LPWizardConstraint accesConstraint = lpw.addConstraint("access" + i + "," + j, 0, "<=");
+                accesConstraint.plus("x" + i + "," + j, -1).setAllVariablesBoolean();
                 for (int k = 0; k < graph.getLength(); k++) {
                     if (graph.isAccessible(i, k))
-                        tmp.plus("x" + k + "," + Integer.toString(j - 1)).setAllVariablesBoolean();
+                        accesConstraint.plus("x" + k + "," + Integer.toString(j - 1)).setAllVariablesBoolean();
                 } // setup for can you access i in j+1 depending of xi,j (there are vertices
                   // not allowed at all)
             }
-            LPWizardConstraint tmp = lpw.addConstraint("maxz" + i, 0, "<=");
-            int signe = 1;
-            if (graph.getValue(i) < 0)
-                signe = -1;
-            else
-                signe = 1;
-            // we must change the sign of the equation depending of the effect of zi on the
-            // objective functions
 
-            tmp.plus("z" + i, -1 * signe).setAllVariablesBoolean();
-            for (int j = 0; j < maxSize; j++) {// zi is the max for j of xi,j
-                tmp.plus("x" + i + "," + j, signe).setAllVariablesBoolean();
+            if (graph.getValue(i) > 0) {
+                LPWizardConstraint maxZConstraint = lpw.addConstraint("maxz" + i, 0, "<=");
+                maxZConstraint.plus("z" + i, -1).setAllVariablesBoolean();
+                for (int j = 0; j < maxSize; j++) {// zi is the max for j of xi,j
+                    maxZConstraint.plus("x" + i + "," + j).setAllVariablesBoolean();
+                }
+            } else {
+                for (int j = 0; j < maxSize; j++) {
+                    lpw.addConstraint("maxz" + i + "," + j, 0, "<=").plus("z" + i).plus("x" + i + "," + j, -1);
+                }
             }
+            // depending of the sign of zi in the objective function the constraint is not
+            // the same
+
         }
         for (int j = 0; j < maxSize - 1; j++) {
             // if the route is ended you can't add new points to the route
             // you can't go in 2 directions idest for j fixed only one xi,j is
             // equals to one
-            LPWizardConstraint tmp = lpw.addConstraint("end" + j, 0, "<=");
-            LPWizardConstraint tmp2 = lpw.addConstraint("one route" + j, 1, ">=");
+            LPWizardConstraint endConstraint = lpw.addConstraint("end" + j, 0, "<=");
+            LPWizardConstraint routeConstraint = lpw.addConstraint("one route" + j, 1, ">=");
             for (int i = 0; i < graph.getLength(); i++) {
-                tmp.plus("x" + i + "," + j).plus("x" + i + "," + Integer.toString(j + 1), -1).setAllVariablesBoolean();
-                tmp2.plus("x" + i + "," + j).setAllVariablesBoolean();
+                endConstraint.plus("x" + i + "," + j).plus("x" + i + "," + Integer.toString(j + 1), -1)
+                        .setAllVariablesBoolean();
+                routeConstraint.plus("x" + i + "," + j).setAllVariablesBoolean();
             }
         }
         lpw.setAllVariablesInteger();// PLNE
-        System.out.println(lpw.getLP().convertToCPLEX().toString());
         return lpw.solve();
     }
 }
